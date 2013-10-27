cf173867e3d9f3d40b81222918278e79fe032e08
Raina Carter <rainacarter@unknownb8e8561882a2.att.net>
2013-10-26 17:15:10 -0700

    clean all the things

--- web.py
+++ web.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
-import os
+from flask import Flask, render_template
 import foodtrucks
 import json
 
@@ -9,7 +8,7 @@ app.config['DEBUG'] = True
 @app.route("/")
 def map():
     # call function to scrape website and put in json format
-    trucks_json =json.dumps(foodtrucks.get_closest_food_trucks())
+    trucks_json = json.dumps(foodtrucks.get_closest_food_trucks())
     return render_template("map.html", trucks=trucks_json)
 
 if __name__ == "__main__":
